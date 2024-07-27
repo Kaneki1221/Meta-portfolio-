20852d2b7f62168d102397d91c743c19b2764fee
kenkaneki <sumith2107@gmail.com>
2024-07-27 05:45:28 +0530

    code fix

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-// script.js
+document.addEventListener("DOMContentLoaded", function() {
+    const elements = document.querySelectorAll('.animate__animated');
 
-document.addEventListener('DOMContentLoaded', () =>
-    document.addEventListener("DOMContentLoaded", function() {
-        const elements = document.querySelectorAll('.animate__animated');
-    
-        elements.forEach((el, index) => {
-            setTimeout(() => {
-                el.classList.remove('animate__animated');
-            }, (index + 1) * 2000);
-        });
+    elements.forEach((el, index) => {
+        setTimeout(() => {
+            el.classList.add('animate__fadeInUp');
+        }, (index + 1) * 1000);
     });
-    
+
+    // Form validation
+    (function () {
+        'use strict'
+        var forms = document.querySelectorAll('.needs-validation')
+        Array.prototype.slice.call(forms)
+            .forEach(function (form) {
+                form.addEventListener('submit', function (event) {
+                    if (!form.checkValidity()) {
+                        event.preventDefault()
+                        event.stopPropagation()
+                    }
+                    form.classList.add('was-validated')
+                }, false)
+            })
+    })()
+});
